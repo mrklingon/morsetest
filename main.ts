7dcb29bd1eadccde53223d8887bd7503aa2133da
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2022-02-23 16:07:28 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,27 @@
-input.touchD0.onEvent(ButtonEvent.Up, function () {
-    encrypt(Isaiah263)
+input.touchD2.onEvent(ButtonEvent.Up, function () {
+    strip.setAll(0x000000)
+    encrypt(verses[vnum])
+})
+input.touchD1.onEvent(ButtonEvent.Up, function () {
+    strip.showAnimation(light.rainbowAnimation, 500)
+    vnum += 1
+    if (vnum > 4) {
+        vnum = 0
+    }
 })
 function doDash () {
     pixel.setColor(0x0000ff)
     pause(200)
     pixel.setColor(0x000000)
+    strip.setPixelColor(1, 0x0000ff)
+    strip.setPixelColor(2, 0x0000ff)
+    shift_strip()
 }
 function encrypt (text: string) {
     for (let index2 = 0; index2 <= text.length - 1; index2++) {
         chr = text.charAt(index2)
         code = morse[alphabet.indexOf(chr)]
         playCode(code)
-        doSpace()
         pause(500)
     }
 }
@@ -19,6 +29,18 @@ function doDot () {
     pixel.setColor(0xffff00)
     pause(100)
     pixel.setColor(0x000000)
+    strip.setPixelColor(1, 0xffff00)
+    shift_strip()
+}
+function shift_strip () {
+    for (let index = 0; index <= 25; index++) {
+        shft = 25 - index
+        strip.setPixelColor(shft + 4, strip.pixelColor(shft))
+    }
+    strip.setPixelColor(0, 0x000000)
+    strip.setPixelColor(1, 0x000000)
+    strip.setPixelColor(2, 0x000000)
+    strip.setPixelColor(3, 0x000000)
 }
 function playCode (text: string) {
     for (let index = 0; index <= text.length - 1; index++) {
@@ -73,7 +95,7 @@ function doINIT () {
     "---..",
     "----.",
     "-----",
-    "/"
+    " "
     ]
     j316 = "for god so loved the world"
     P23 = "the lord is my shepherd"
@@ -139,21 +161,30 @@ function doSpace () {
     pixel.setColor(0x00ff00)
     pause(200)
     pixel.setColor(0x000000)
+    strip.setPixelColor(0, 0x00ff00)
+    strip.setPixelColor(1, 0x000000)
+    strip.setPixelColor(2, 0x000000)
+    shift_strip()
 }
 let vn = ""
 let VNames: string[] = []
-let verses: string[] = []
 let _2Tim316 = ""
+let Isaiah263 = ""
 let Phl4 = ""
 let P23 = ""
 let j316 = ""
 let verse = ""
+let shft = 0
 let alphabet: string[] = []
 let morse: string[] = []
 let code = ""
 let chr = ""
-let Isaiah263 = ""
+let verses: string[] = []
+let vnum = 0
+let strip: light.NeoPixelStrip = null
 pixel.setColor(0xff0000)
 doINIT()
-pause(100)
+pause(1000)
 pixel.setColor(0x000000)
+strip = light.createStrip(pins.A2, 30)
+vnum = 0
